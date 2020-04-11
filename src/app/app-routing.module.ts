3e824ed8953c6427cc7dd8533cc2b09963ccb29b
Roshan Lasiwa <roshanlasiwa@gmail.com>
2020-04-11 19:21:34 +0545

    added root path in router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from "../app/components/table/table.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component:TableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
